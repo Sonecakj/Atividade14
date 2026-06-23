d921e90b76364799756195374dfddf495782d4f9
Soneca3 <pedrov.r.gonzaga@gmail.com>
2026-06-23 16:34:55 -0300

    Add files via upload

---
+++ exercicio05.js
@@ -0,0 +1,18 @@
+let codigo = [];
+let encontrado = false;
+
+for(let contador = 0; contador < 10; contador++){
+    codigo[contador] = Number(prompt(`Digite o valor da posição ${contador}`));
+    
+}
+let busca = Number(prompt("Qual código você deseja rastrear?"));
+
+for(let contador = 0; contador <10; contador++){
+    if(codigo[contador] === busca){
+        console.log(`Código encontrado na ${contador}° posição`);
+        encontrado = true
+    }
+}
+else if(encontrado === false){
+        console.log(`Código não encontrado`);
+    }
